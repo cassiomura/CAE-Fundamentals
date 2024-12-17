cd2b64a00994ac1a76c8b72f41285ad6b9aa9c40
Cássio Murakami <cassio.mura@hotmail.com>
2024-12-17 10:29:55 -0300

    Update addition solver methods

--- SolverCAE.py
+++ SolverCAE.py
@@ -12,21 +12,65 @@ from ElementLibrary.Tridimensional.CTETRA import CTETRA
 from ElementLibrary.Tridimensional.CHEXA import CHEXA
 from ElementLibrary.FiniteElement import FiniteElement
 
-def solve_system(stiffness_matrix_bcs: np.array, force_vector_bcs: np.array, stiffness_matrix: np.array) -> tuple:
-    # Solve for the displacement vector using the stiffness matrix and force vector
-    displacement_vector = np.linalg.solve(stiffness_matrix_bcs, force_vector_bcs)
-    displacement_vector = displacement_vector.flatten()
-    
-    # Solve for the force vector:
-    force_vector = np.matmul(stiffness_matrix, displacement_vector)
-     
+def solve_linear_static_analysis(method: str, simulation_case: str, df_nodes: pd.DataFrame, df_boundary_conditions: pd.DataFrame, stiffness_matrix: np.array, force_vector: np.array):
+    fixed_dofs = get_boundary_conditions_dofs(simulation_case, df_nodes, df_boundary_conditions)
+
+    if method == "Penalty Method":
+        displacement_vector, force_vector = solve_system_penalty_method(stiffness_matrix, force_vector, fixed_dofs)
+    elif method == "Reduction Method":
+        displacement_vector, force_vector = solve_system_reduction_method(stiffness_matrix, force_vector, fixed_dofs)
+    else:
+        raise ValueError(f"Unsupported method: '{method}'. Please use 'Penalty Method' or 'Reduction Method'.")
+
     return displacement_vector, force_vector
 
-def assemble_global_stiffness_matrix(df_elements: pd.DataFrame, df_nodes: pd.DataFrame, df_materials: pd.DataFrame, df_properties:pd.DataFrame) -> np.array:
-    # Number of nodes in the mesh
-    num_nodes = df_nodes.shape[0] 
+def solve_system_penalty_method(stiffness_matrix: np.array, force_vector: np.array, fixed_dofs: np.array) -> tuple:
+    stiffness_matrix_bcs = apply_penalty_boundary_conditions_stiffness(fixed_dofs, stiffness_matrix)
+    force_vector_bcs = apply_penalty_boundary_conditions_force(fixed_dofs, force_vector)
+
+    # Solve for the displacements
+    displacement_vector = np.linalg.solve(stiffness_matrix_bcs, force_vector_bcs).flatten()
+
+    # Solve for the forces
+    force_vector = np.matmul(stiffness_matrix, displacement_vector)
+
+    return displacement_vector, force_vector
+
+def solve_system_reduction_method(stiffness_matrix: np.array, force_vector: np.array, fixed_dofs: np.array) -> tuple:
+    free_dofs = [i for i in range(len(force_vector)) if i not in fixed_dofs]
+
+    # Stiffness matrix decompostion
+    K_I_I = stiffness_matrix[np.ix_(fixed_dofs, fixed_dofs)]  
+    K_I_II = stiffness_matrix[np.ix_(fixed_dofs, free_dofs)]  
+    K_II_I = stiffness_matrix[np.ix_(free_dofs, fixed_dofs)]  
+    K_II_II = stiffness_matrix[np.ix_(free_dofs, free_dofs)]  
     
-    # Determine number of DOFs based on the first element type
+    # Force vector decomposition
+    F_I = force_vector[fixed_dofs]
+    F_II = force_vector[free_dofs]
+
+    # Initialization of the solution
+    displacement_vector = np.zeros_like(force_vector)
+    force_vector_ = np.zeros_like(force_vector)
+
+    # Solve displacements at free dofs
+    U_II = np.linalg.solve(K_II_II, F_II)
+    
+    # Solve forces at fixed dofs
+    F_I_updated = np.matmul(K_I_II, U_II)
+
+    # Reconstruct the displacement vector
+    displacement_vector[free_dofs] = U_II  
+    displacement_vector[fixed_dofs] = 0  # Fixed dofs remain zero
+
+    # Reconstruct the force vector
+    force_vector_[fixed_dofs] = F_I_updated
+    force_vector_[free_dofs] = F_II  # Free dofs forces remain unchanged
+
+    return displacement_vector.flatten(), force_vector_.flatten()
+
+def assemble_global_stiffness_matrix(df_elements: pd.DataFrame, df_nodes: pd.DataFrame, df_materials: pd.DataFrame, df_properties:pd.DataFrame) -> np.array:
+    num_nodes = df_nodes.shape[0] 
     num_dofs_per_node = FiniteElement.DOF_PER_NODE_MAP.get(df_elements.iloc[0, 0])  
     num_dofs = num_dofs_per_node * num_nodes
 
@@ -35,14 +79,13 @@ def assemble_global_stiffness_matrix(df_elements: pd.DataFrame, df_nodes: pd.Dat
 
     # Iterate through each element in the mesh
     for _, row in df_elements.iterrows():
-        # Create element object based on its type
         element_type = row['Element Type']
-        if element_type == 'CTRIA3':
+        if element_type == 'CBAR':
+            element = CBAR(row, df_nodes, df_properties, df_materials)
+        elif element_type == 'CTRIA3':
             element = CTRIA3(row, df_nodes, df_properties, df_materials)
         elif element_type == 'CQUAD4':
             element = CQUAD4(row, df_nodes, df_properties, df_materials)
-        elif element_type == 'CBAR':
-            element = CBAR(row, df_nodes, df_properties, df_materials)
         elif element_type == 'CTETRA':
             element = CTETRA(row, df_nodes, df_properties, df_materials)
         elif element_type == 'CHEXA':
@@ -60,16 +103,13 @@ def assemble_global_stiffness_matrix(df_elements: pd.DataFrame, df_nodes: pd.Dat
     return K_global
 
 def assemble_global_load_vector(simulation_case: str, df_nodes: pd.DataFrame, df_loads: pd.DataFrame) -> np.array:
-    # Number of nodes in the mesh
     num_nodes = df_nodes.shape[0] 
-    
-    # Determine number of DOFs based on the first element type
     num_dofs_per_node = FiniteElement.DOF_PER_NODE_MAP.get(simulation_case)  
     num_dofs = num_dofs_per_node * num_nodes
 
     # Initialize the global force vector
     F_global = np.zeros((num_dofs, 1))
-    # Loop for every force in the loads dataframe:
+
     for _, row in df_loads.iterrows():
         load_type = row['Type']
         node_id = int(row['G'])
@@ -91,27 +131,30 @@ def assemble_global_load_vector(simulation_case: str, df_nodes: pd.DataFrame, df
             F_global[dof] += force_magnitude * normal[i]
     
     return F_global
-    
-def impose_boundary_conditions_penalty_method(simulation_case: str, df_nodes: pd.DataFrame, df_boundary_conditions: pd.DataFrame, stiffness_matrix: np.array, force_vector: np.array) -> tuple:
-    # Retrieve the list of degrees of freedom that are fixed based on boundary conditions
-    fixed_dofs = get_boundary_conditions_dofs(simulation_case, df_nodes, df_boundary_conditions)
-    
-    # Set the fixed boundary conditions in the stiffness matrix and force vector
-    updated_stiffness_matrix, updated_force_vector = set_fixed_boundary_conditions(fixed_dofs, stiffness_matrix, force_vector)
 
-    return updated_stiffness_matrix, updated_force_vector
+def apply_penalty_boundary_conditions_stiffness(fixed_dofs: np.array, stiffness_matrix: np.array) -> tuple:
+    stiffness_matrix_ = stiffness_matrix.copy()
+
+    for fixed_dof in fixed_dofs:
+        stiffness_matrix_[fixed_dof, :] = 0
+        stiffness_matrix_[:, fixed_dof] = 0
+        stiffness_matrix_[fixed_dof, fixed_dof] = 1
+
+    return stiffness_matrix_
+
+def apply_penalty_boundary_conditions_force(fixed_dofs: np.array, force_vector: np.array) -> tuple:
+    force_vector_ = force_vector.copy()
+
+    for fixed_dof in fixed_dofs:
+        force_vector_[fixed_dof] = 0
+    
+    return force_vector_
 
 def get_boundary_conditions_dofs(simulation_case: str, df_nodes: pd.DataFrame, df_boundary_conditions: pd.DataFrame) -> np.array:
-    # Number of nodes in the mesh
     num_nodes = df_nodes.shape[0] 
-
-    # Determine number of DOFs based on the first element type
     num_dofs_per_node = FiniteElement.DOF_PER_NODE_MAP.get(simulation_case) 
 
-    # List to store fixed DOFs
     fixed_dofs = []
-
-    # Iterate over each boundary condition
     for _, row in df_boundary_conditions.iterrows():
         dof_types = row['C'] # Extract DOF types
         nodes = row['G'].split() # Extract node IDs
@@ -126,22 +169,4 @@ def get_boundary_conditions_dofs(simulation_case: str, df_nodes: pd.DataFrame, d
                 if dof_index < num_dofs_per_node:  # Ensure dof_index does not surpass num_dofs_per_node
                     fixed_dofs.append(node_index + dof_index*num_nodes)
 
-    # Return fixed DOFs as a NumPy array
-    return np.array(fixed_dofs) 
-
-def set_fixed_boundary_conditions(fixed_dofs: np.array, stiffness_matrix: np.array, force_vector: np.array) -> tuple:
-    # Create a copy of the global stiffness matrix and global force vector 
-    stiffness_matrix_ = stiffness_matrix.copy()
-    force_vector_ = force_vector.copy()
-
-    # Iterate over each fixed degree of freedom
-    for fixed_dof in fixed_dofs:
-        # Set the corresponding row and column in the stiffness matrix to zero (except for the diagonal element)
-        stiffness_matrix_[fixed_dof, :] = 0
-        stiffness_matrix_[:, fixed_dof] = 0
-        stiffness_matrix_[fixed_dof, fixed_dof] = 1  # Set the diagonal element to 1
-
-        # Set the corresponding force in the force vector to zero
-        force_vector_[fixed_dof] = 0
-
-    return stiffness_matrix_, force_vector_
+    return np.array(fixed_dofs)
